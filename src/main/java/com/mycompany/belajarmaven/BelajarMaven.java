0742bfe530a42900492a513ab0060b5f13c7dda5
Suka Hermawan net <sukahermawan877@gmail.com>
2025-02-14 03:32:27 +0000

    upload pertama

---
+++ src/main/java/com/mycompany/belajarmaven/BelajarMaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.belajarmaven;
+
+/**
+ *
+ * @author sukah
+ */
+public class BelajarMaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
